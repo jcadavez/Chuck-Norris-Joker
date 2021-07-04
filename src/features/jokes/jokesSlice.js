022dd89530dcded9a83b462fa6bc49fbbd3e07e6
Joshua Cadavez <jcadavez@insider.com>
2021-07-04 01:17:54 -0400

    Setup store, provider, and joke slice init state

---
+++ src/features/jokes/jokesSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+    { 
+        id: '1', 
+        value: 'Everybody thinks the Galaxy Note 7 is explosive. In fact it is only Chuck Norris who tries to send a WhatsApp message with a selfie to his fans.', 
+        url: 'https://api.chucknorris.io/jokes/H9ASnHczQBG0Dj_a7eoRhw' 
+    },
+    { 
+        id: '2',
+        value: 'Chuck Norris uspio uraditi zadaću da mu prođu svi autotestovi troll:',
+        url: 'https://api.chucknorris.io/jokes/AnBWx135Qf2t-CF9DFMiLw'
+    }
+]
+
+const jokesSlice = createSlice({
+    name: 'jokes',
+    initialState,
+    reducers: {}
+})
+
+export default jokesSlice.reducer
